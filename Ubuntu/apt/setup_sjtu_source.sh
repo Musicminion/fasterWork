b4df3d44fb7837f962b642d3f2b206c50be76ead
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2023-05-10 04:47:09 +0000

    feat: ubuntu arm support

--- Ubuntu/apt/setup_sjtu_source.sh
+++ Ubuntu/apt/setup_sjtu_source.sh
@@ -9,15 +9,30 @@ fi
 # 备份当前源列表
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-# 更新软件源为上海交通大学镜像站的源
-sudo tee /etc/apt/sources.list << EOF
-deb https://mirrors.sjtug.sjtu.edu.cn/ubuntu/ $(lsb_release -cs) main restricted universe multiverse
-deb https://mirrors.sjtug.sjtu.edu.cn/ubuntu/ $(lsb_release -cs)-updates main restricted universe multiverse
-deb https://mirrors.sjtug.sjtu.edu.cn/ubuntu/ $(lsb_release -cs)-backports main restricted universe multiverse
-deb https://mirrors.sjtug.sjtu.edu.cn/ubuntu/ $(lsb_release -cs)-security main restricted universe multiverse
-EOF
+#  获取系统架构信息
+ARCH=$(uname -m)
 
-# 更新软件包列表
-sudo apt update
+# 根据系统架构信息，选择添加的源
+if [ ${ARCH} = "x86_64" ]; then
+    # 更新软件源为上海交通大学镜像站的源
+    sudo tee /etc/apt/sources.list << EOF
+    deb https://mirror.sjtu.edu.cn/ubuntu/ $(lsb_release -cs) main restricted universe multiverse
+    deb https://mirror.sjtu.edu.cn/ubuntu/ $(lsb_release -cs)-updates main restricted universe multiverse
+    deb https://mirror.sjtu.edu.cn/ubuntu/ $(lsb_release -cs)-backports main restricted universe multiverse
+    deb https://mirror.sjtu.edu.cn/ubuntu/ $(lsb_release -cs)-security main restricted universe multiverse
+EOF
+    # 更新软件包列表
+    sudo apt update
+else
+    # 更新软件源为上海交通大学镜像站的port源
+    sudo tee /etc/apt/sources.list << EOF
+    deb https://mirror.sjtu.edu.cn/ubuntu-ports/ $(lsb_release -cs) main restricted universe multiverse
+    deb https://mirror.sjtu.edu.cn/ubuntu-ports/ $(lsb_release -cs)-updates main restricted universe multiverse
+    deb https://mirror.sjtu.edu.cn/ubuntu-ports/ $(lsb_release -cs)-backports main restricted universe multiverse
+    deb https://mirror.sjtu.edu.cn/ubuntu-ports/ $(lsb_release -cs)-security main restricted universe multiverse
+EOF
+    # 更新软件包列表
+    sudo apt update
+fi
 
 echo "源已经更改为上海交通大学镜像站的源。"
